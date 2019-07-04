a7b34a150a3d93a08fc91845e652f1b84d555656
Matheus Almeida <mat.almeida@live.com>
2019-07-04 18:50:07 -0300

    fix(config): site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    title: 'Matheus Almeida - Programmer and Creative',
+    author: 'Matheus Almeida',
+    description: 'Based on a Gatsby V2 starter Dimension by HTML5 UP',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
